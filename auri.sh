e74d8db580c4584e5f5eba59da35a77524d9d460
Piotr Wittchen <piotr@wittchen.io>
2019-07-07 11:56:56 +0200

    adding searching and fetching packages

--- auri.sh
+++ auri.sh
@@ -13,12 +13,24 @@ function help() {
 }
 
 function search() {
-  if [ -z "$1" ]; then
+  if [ -z "$1" ] ; then
     echo "you haven't provided package name!"
     exit
   fi
 
   echo "searching for a package $1"
+  output=$(curl "https://aur.archlinux.org/rpc?type=suggest&arg=$1" -s | jq '.[]')
+  if [ -z "$output" ] ; then
+    echo "nothing was found"
+  else
+    echo "the following candidates were found:"
+    output_array=($output)
+    for i in "${output_array[@]}"
+    do
+        :
+        echo $i | sed -e 's/^"//' -e 's/"$//'
+    done
+  fi
 }
 
 function fetch() {
@@ -28,6 +40,16 @@ function fetch() {
   fi
 
   echo "fetching package $1"
+  output=$(curl "https://aur.archlinux.org/rpc?type=suggest&arg=$1" -s | jq '.[]')
+
+  if [ -z "$output" ] ; then
+    echo "there's not such package in aur"
+    exit
+  fi
+
+  mkdir -p "/tmp/auri/$1"
+  git clone "https://aur.archlinux.org/$1.git" "/tmp/auri/$1"
+  echo "done"
 }
 
 function install() {
